bc33ea89d8f9b1d3c0cf36602909090673e07df4
Rene Sorel <rene@proversity.org>
2017-03-14 15:39:07 +0200

    test broken iter

--- recap/recap.py
+++ recap/recap.py
@@ -106,7 +106,7 @@ class RecapXBlock(XBlock, StudioEditableXBlockMixin, XBlockWithSettingsMixin):
         """
         blocks = []
         for block, xblock_type in self.get_blocks(self.xblock_list):
-            logger.info(u'XBLOCK LIST `{}`: "{}"'.format(block, xblock_type))
+            logger.info(u'XBLOCK LIST {}: "{}"'.format(block, xblock_type))
             question, answer = self.get_field_names(xblock_type)
             blocks.append((getattr(block, question), getattr(block, answer)))
 
