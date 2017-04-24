9559a7a5abe77c625458c9aea1148d889bb08d04
Jose Antonio Gonzalez <antonio@proversity.org>
2017-04-24 16:20:43 +0200

    build ids

--- recap/recap.py
+++ recap/recap.py
@@ -219,7 +219,7 @@ class RecapXBlock(XBlock, StudioEditableXBlockMixin, XBlockWithSettingsMixin):
         frag.add_javascript_url(self.runtime.local_resource_url(self, 'public/jsPDF-1.3.2/jspdf.min.js'))
         frag.add_javascript(self.resource_string("static/js/src/recap.js"))
         frag.initialize_js('RecapXBlock', {
-            'recap_cmd_id': 'recap_cmd_id' + self.scope_ids.usage_id._to_string(),
+            'recap_cmd_id': 'recap_cmd_' + self.scope_ids.usage_id._to_string(),
             'recap_actions_id': 'recap_actions_' + self.scope_ids.usage_id._to_string(),
             'recap_editor_id': 'recap_editor_' + self.scope_ids.usage_id._to_string(),
         })
