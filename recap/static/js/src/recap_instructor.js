e670b0d7c2e3db775026328142ab7a125d051a8f
lidijarad <radovanovic.lidija22@gmail.com>
2017-06-13 14:08:56 +0200

    add some ids

--- recap/static/js/src/recap_instructor.js
+++ recap/static/js/src/recap_instructor.js
@@ -1,13 +1,17 @@
 /* Javascript for OpenResponseXBlock. */
-function RecapDashboard(runtime, element) {
+function RecapDashboard(runtime, element, data) {
 
     $(function ($) {
+    	$('#recap-table tr button').click(function (event) {
+          alert($(this).attr('id')); 
 
-        $('#download').click(function(eventObject) {
+    	var user_id = '#' + data.user_id;
+
+        $('#').click(function(eventObject) {
             console.log('hiii');
             var handlerUrl = runtime.handlerUrl(element, 'generate_pdf');
             //var answer_html = $('#student-input').val();
-            var recap_answers = '#' + data.recap_answers_id;
+            var recap_answers = "THIS IS MY TEST STRING";
             console.log(recap_answers)
             eventObject.preventDefault();
             $.ajax({
