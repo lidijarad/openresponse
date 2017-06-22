2c0e49d3d63d34cac1744d781729a74e5ed03261
lidijarad <radovanovic.lidija22@gmail.com>
2017-06-22 08:08:54 +0200

    add date pdf name, need to add username

--- recap/public/recap_instructor.js
+++ recap/public/recap_instructor.js
@@ -2,12 +2,19 @@
 /* Javascript for RecapDashboard. */
 (
     function RecapDashboard(runtime, element, data) {
+        
+        current_date = new Date();
+        month = current_date.getMonth() + 1;
+        pdf_name = ''
+        pdf_name = 'recap' + String(current_date.getDate()) + '/' + String(month) + '/' + String(current_date.getFullYear()) + '.pdf';  
 
-         $('.download_answer').click(function(event) {
-            var pdf_element = "TEST"
+        $('.download_answer').click(function(event) {
+            
+            var pdf_element = $(this).closest('td').prev('.ans').html();
+
             html2pdf(pdf_element, {
               margin: [0.8, 1, 0.5, 1],
-              filename: 'testInstructor.pdf',
+              filename: pdf_name,
               image: { type: 'jpeg',quality: 0.98 },
               html2canvas: { dpi: 192, letterRendering: true },
               jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' }
