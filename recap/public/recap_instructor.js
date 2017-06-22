2a1b474f0cb8b9992da99f92012474db74c79346
lidijarad <radovanovic.lidija22@gmail.com>
2017-06-22 11:44:46 +0200

    add correct pdf name

--- recap/public/recap_instructor.js
+++ recap/public/recap_instructor.js
@@ -6,7 +6,8 @@
         current_date = new Date();
         month = current_date.getMonth() + 1;
         pdf_name = ''
-        pdf_name = 'recap' + String(current_date.getDate()) + '/' + String(month) + '/' + String(current_date.getFullYear()) + '.pdf';  
+        pdf_name =  String(current_date.getDate()) + '/' + String(month) + '/' + String(current_date.getFullYear());
+
 
         // Try add pagination
 
@@ -48,10 +49,12 @@
             
             var pdf_element_id = $(this).closest('td').prev('.ans').attr('id');
             var pdf_element = document.getElementById(String(pdf_element_id)).innerHTML;
+            var pdf_name_user = pdf_name + '_' + String(pdf_element_id) + '.pdf'
+
 
             html2pdf(pdf_element, {
               margin: [0.8, 1, 0.5, 1],
-              filename: pdf_name,
+              filename: pdf_name_user,
               image: { type: 'jpeg',quality: 0.98 },
               html2canvas: { dpi: 192, letterRendering: true },
               jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' }
