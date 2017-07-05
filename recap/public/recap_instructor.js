55069af2f58694d7cc4b0f7040f08a2a82709a95
lidijarad <radovanovic.lidija22@gmail.com>
2017-07-05 14:31:04 +0200

    try some more stuff

--- recap/public/recap_instructor.js
+++ recap/public/recap_instructor.js
@@ -71,12 +71,21 @@
         $('#notifications-btn').click(function(event){
             event.preventDefault();
             event.stopImmediatePropagation()
+            console.log('I was clicked');
             var noteFormUrl;
             noteFormUrl = $('#notifications-frm').attr('action');
+            var csrftoken = getCookie('csrftoken');
+            $.ajaxSetup({
+        beforeSend: function(xhr, settings) {
+            if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+              xhr.setRequestHeader("X-CSRFToken", csrftoken);
+            }
+            }
+            });
             $.ajax({
                 url: noteFormUrl,
                 method: 'post',
-                data: {'text': 'hi'},
+                data: {'text': note_text},
                 beforeSend: function(xhr, settings) {
             if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
               xhr.setRequestHeader("X-CSRFToken", csrftoken);
@@ -98,8 +107,25 @@
                 }
             })
         });
-
-
+        function getCookie(name) {
+            var cookieValue = null;
+            if (document.cookie && document.cookie !== '') {
+                var cookies = document.cookie.split(';');
+                for (var i = 0; i < cookies.length; i++) {
+                    var cookie = jQuery.trim(cookies[i]);
+                    // Does this cookie string begin with the name we want?
+                    if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                        break;
+                    }
+                }
+            }
+            return cookieValue;
+        }
+        function csrfSafeMethod(method) {
+            // these HTTP methods do not require CSRF protection
+            return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+        }
 
         $('.download_answer').click(function(event) {
             
