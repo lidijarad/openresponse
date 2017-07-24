d820ae4996ee5f7a02866b34692cfb7ed00d52b8
lidijarad <radovanovic.lidija22@gmail.com>
2017-07-24 12:51:09 +0200

    add search bar and drop down

--- recap/public/recap_instructor.js
+++ recap/public/recap_instructor.js
@@ -45,7 +45,21 @@
             for (var i = nBegin; i <= nEnd; i++) {
               $(tr[i]).show();
             }
-        });  
+        });
+
+
+        $("#search").keyup(function(){
+            _this = this;
+             // Show only matching TR, hide rest of them
+            $.each($("#recap-table tbody tr"), function() {
+                if($(this).text().toLowerCase().indexOf($(_this).val().toLowerCase()) === -1) {
+                    $(this).hide();
+                }
+                else {
+                   $(this).show();
+                } 
+            });
+         });  
 
         // Callback for showing and hiding spinner
         function SpinnerCallback(shouldShowSpinner, cb) {
