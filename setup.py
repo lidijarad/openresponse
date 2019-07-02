a1b601c6aa7c68adab7e62cdb8a1b20633efe928
AndreyCanon <andrey.canon@edunext.co>
2019-07-02 18:59:48 -0500

    Bump version: 0.3.3 → 0.4.0

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def package_data(pkg, roots):
 
 setup(
     name='recap-xblock',
-    version='0.3.3',
+    version='0.4.0',
     description='Recap XBlock',   # TODO: write a better description.
     license='AGPL v3',          # TODO: choose a license: 'AGPL v3' and 'Apache 2.0' are popular.
     packages=[
